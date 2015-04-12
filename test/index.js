846e5f00e9829b24472dd1ea74c1b2beaa3959f1
Nate Goldman <nate@ngoldman.me>
2015-04-12 14:46:26 -0700

    add tests

---
+++ test/index.js
@@ -0,0 +1,23 @@
+var cwp = require('..')
+var test = require('tape')
+var path = require('path')
+var cwd = process.cwd()
+
+test('works as expected', function (t) {
+  t.plan(4)
+
+  var a = cwp('package.json')
+
+  t.equal(a, cwd + '/package.json')
+
+  var b = cwp('test', 'index.js')
+  t.equal(b, cwd + '/test/index.js')
+
+  var c = cwp('lib/secret/dir', 'file')
+  t.equal(c, cwd + '/lib/secret/dir/file')
+
+  var d = cwp('../../../.ssh/id_rsa')
+  t.equal(d, path.join(cwd, '../../../.ssh/id_rsa'))
+
+  t.end()
+})
