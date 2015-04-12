786d50f00c276172457a396e6845081e29e10f2c
Nate Goldman <nate@ngoldman.me>
2015-04-12 14:51:18 -0700

    simplify internal logic

--- index.js
+++ index.js
@@ -2,15 +2,6 @@ var path = require('path')
 
 module.exports = function cwp () {
   var args = Array.prototype.slice.call(arguments)
-  var arr = []
-
-  function add (a) { arr.push(a) }
-
-  args.forEach(function (arg) {
-    if (arg.indexOf('/') !== -1) arg.split('/').forEach(add)
-    else add(arg)
-  })
-
-  arr.unshift(process.cwd())
-  return path.join.apply(null, arr)
+  args.unshift(process.cwd())
+  return path.join.apply(null, args)
 }
